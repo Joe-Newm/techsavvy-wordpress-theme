020f1353088aa49c67b8903389ff95ec2478afe8
Joe-Newm <j.newm500@gmail.com>
2024-12-07 19:36:01 -0600

    add front page code

---
+++ front-page.php
@@ -0,0 +1,53 @@
+
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+  <link rel="stylesheet" href="/wp-content/themes/techsavvy-wordpress-theme/style.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link
+    href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+    rel="stylesheet">
+		<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+		<meta name="generator" content={Astro.generator} />
+		<title>Techsavvy</title>
+    </head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-light container">
+  <a class="navbar-brand" href="./index.html">
+    <img id="logo" src="/wp-content/themes/techsavvy-wordpress-theme/assets/images/horizontal-logo.svg" alt="techsavvy logo">
+  </a>
+  <!-- Hamburger Button -->
+  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <!-- Menu -->
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <div class="navbar-nav ml-auto">
+      <a class="nav-item nav-link" href="./services.html">Services</a>
+      <a class="nav-item nav-link" href="./about.html">About Us</a>
+      <a class="nav-item nav-link" href="./contact">Contact Us</a>
+      <a class="nav-item nav-link" href="./careers">Careers</a>
+      <button class="btn btn-primary" type="button" onclick="window.location.href='tel:+13188840844';">
+        <i class="bi bi-telephone-fill" style="font-size: 1.5em"></i>(318) 884-0844
+      </button>
+    </div>
+  </div>
+        </nav>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+  integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+  crossorigin="anonymous"></script>
+
+    </body>
+
+    </html>
